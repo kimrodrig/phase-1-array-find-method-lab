04ae9c8edb00ed251a8319c8411e81f7287f6a82
Kim <rodrigkim@gmail.com>
2022-08-30 12:42:04 -0400

    commit tree f9ab2623dbd8ea4dd6df104178e2f55a92564592

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function isWin(element){
+    if (element.result == "W"){
+        return element.year
+    }
+}
+
+function superbowlWin(array){
+    const finder = array.find(isWin);
+    if (finder !== undefined){
+        return finder["year"]
+    }
+    else{
+        return finder
+    }
+}
